31d103a49bedf93ba250ff4dfd240a7593251a97
Facundo Fuentes <71295352+FacundoFuentes@users.noreply.github.com>
2022-08-22 11:54:01 -0300

    Updated app.module with cities resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CitiesModule } from './cities/cities.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://facufuentes:BKd6VeT5jtosZCMr@cluster0.eorhy.mongodb.net/backend-challenge?retryWrites=true&w=majority',
+    ),
+    CitiesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
