26e87af3523aaed5810038279f06519b7fe364de
Facundo Fuentes <71295352+FacundoFuentes@users.noreply.github.com>
2022-08-22 12:30:39 -0300

    Updated parameter type

--- src/cities/cities.service.ts
+++ src/cities/cities.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { CreateCityDto } from './dto/create-city.dto';
 import { City, CityDocument } from './schemas/cities.schema';
 
 @Injectable()
 export class CitiesService {
   constructor(@InjectModel(City.name) private cityModel: Model<CityDocument>) {}
 
-  async createCity(city: City): Promise<CityDocument> {
+  async createCity(city: CreateCityDto): Promise<CityDocument> {
     const newCity = new this.cityModel(city);
     return newCity.save();
   }
