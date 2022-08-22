355bdfd78d711193f38358788481bd3242d51d48
Facundo Fuentes <71295352+FacundoFuentes@users.noreply.github.com>
2022-08-22 12:39:57 -0300

    Created controller for city creation

--- src/cities/cities.controller.ts
+++ src/cities/cities.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { CitiesService } from './cities.service';
+import { CreateCityDto } from './dto/create-city.dto';
+import { CityDocument } from './schemas/cities.schema';
 
 @Controller('cities')
 export class CitiesController {
   constructor(private readonly citiesService: CitiesService) {}
+
+  @Post('/create')
+  async createCity(
+    @Res() response,
+    @Body() city: CreateCityDto,
+  ): Promise<CityDocument> {
+    const newCity = await this.citiesService.createCity(city);
+    return response.status(200).json(newCity);
+  }
 }
