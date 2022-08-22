275d218f93ccc7354d4afb475814346ade514314
Facundo Fuentes <71295352+FacundoFuentes@users.noreply.github.com>
2022-08-22 12:17:08 -0300

    Created function to add cities

--- src/cities/cities.service.ts
+++ src/cities/cities.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { City, CityDocument } from './schemas/cities.schema';
 
 @Injectable()
-export class CitiesService {}
+export class CitiesService {
+  constructor(@InjectModel(City.name) private cityModel: Model<CityDocument>) {}
+
+  async createCity(city: City): Promise<CityDocument> {
+    const newCity = new this.cityModel(city);
+    return newCity.save();
+  }
+}
