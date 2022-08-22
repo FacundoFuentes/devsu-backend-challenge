2a0f9b18e3e6b8193c0aad47a3f0a14365fe95a9
Facundo Fuentes <71295352+FacundoFuentes@users.noreply.github.com>
2022-08-22 16:03:58 -0300

    Edited path to file

--- src/utils/csv-to-json.ts
+++ src/utils/csv-to-json.ts
@@ -1,4 +1,4 @@
-const csvFilePath = 'src/data/cities_canada-usa.csv';
+const csvFilePath = `${process.cwd()}/src/data/cities_canada-usa.csv`;
 import csv from 'csvtojson';
 export async function csvToJson() {
   const jsonArray = await csv().fromFile(csvFilePath);
