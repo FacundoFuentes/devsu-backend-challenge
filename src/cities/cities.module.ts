22ea29595d5588975a7fc0035e9563d947eacf98
Facundo Fuentes <71295352+FacundoFuentes@users.noreply.github.com>
2022-08-22 12:13:47 -0300

    Added schema to cities.module

--- src/cities/cities.module.ts
+++ src/cities/cities.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CitiesService } from './cities.service';
 import { CitiesController } from './cities.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { City } from './schemas/cities.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: City.name, schema: City }])],
   controllers: [CitiesController],
-  providers: [CitiesService]
+  providers: [CitiesService],
 })
 export class CitiesModule {}
