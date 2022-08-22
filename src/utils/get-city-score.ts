95b9f7082f1bae36b14a467536b02777b76cdf17
Facundo Fuentes <71295352+FacundoFuentes@users.noreply.github.com>
2022-08-22 15:56:23 -0300

    Created function to get scores

---
+++ src/utils/get-city-score.ts
@@ -0,0 +1,32 @@
+import { quickScore } from 'quick-score';
+
+export function formatAndScore(cities, query) {
+  console.log(cities, query);
+
+  const suggestions = [];
+  let finalScore = 0;
+  cities.map((city) => {
+    if (!query.latitude || !query.longitude) {
+      finalScore = quickScore(city.name, query.name);
+      console.log(quickScore(city.name, query.name));
+    } else {
+      finalScore =
+        (quickScore(city.long, query.longitude) +
+          quickScore(city.lat, query.latitude) +
+          quickScore(city.name, query.name)) /
+        3;
+    }
+    if (finalScore !== 0 && finalScore !== 1) {
+      finalScore = Number(finalScore.toFixed(2));
+    }
+
+    suggestions.push({
+      name: `${city.name} ${city.country} ${city.admin1}`,
+      latitude: city.lat,
+      longitude: city.long,
+      score: finalScore,
+    });
+  });
+
+  return suggestions;
+}
